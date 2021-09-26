7f2b359a71eefdd5c9c8bf9e45078f506aff0f6e
Jose D'Afonseca <josepfj1@gmail.com>
2021-09-26 11:09:37 -0300

    feat: implamentation tasklistadapter

---
+++ app/src/main/java/com/jose/todopriority/adapter/TaskListAdapter.kt
@@ -0,0 +1,65 @@
+package com.jose.todopriority.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.jose.todopriority.R
+import com.jose.todopriority.databinding.ItemTaksBinding
+import com.jose.todopriority.model.Task
+
+class TaskListAdapter: ListAdapter<Task, TaskListAdapter.TaskViewHolder>(DiffCallback()) {
+
+    var listenerEdit: (Task) -> Unit = {}
+    var listenerDelete: (Task) -> Unit = {}
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TaskListAdapter.TaskViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemTaksBinding.inflate(inflater, parent, false)
+        return TaskViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TaskListAdapter.TaskViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class TaskViewHolder(
+        private val binding: ItemTaksBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Task){
+            binding.tvTitle.text = item.title
+            binding.tvDate.text = item.date
+            binding.tvDescription.text = item.description
+            binding.ivMore.setOnClickListener {
+                showPopup(item)
+            }
+        }
+
+        fun showPopup(item: Task) {
+            val ivMore = binding.ivMore
+            val popupMenu = PopupMenu(ivMore.context, ivMore)
+            popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.action_edit -> listenerEdit(item)
+                    R.id.action_delete -> listenerDelete(item)
+
+                }
+                return@setOnMenuItemClickListener true
+            }
+            popupMenu.show()
+        }
+    }
+}
+
+class DiffCallback: DiffUtil.ItemCallback<Task>() {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean = oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean = oldItem.id == newItem.id
+
+}
